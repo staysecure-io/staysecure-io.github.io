b0dec40a1d9659a4cff74ab03d317c2e62e17b57
Alexey Busygin <alexey@busygin.com>
2024-08-09 17:45:25 +0200

    StaySecure front page

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+export default defineConfig({
+  root: 'src', // Set the root to the src folder
+  build: {
+    outDir: '../dist', // Output the build files outside the src folder
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'src/index.html',
+        privacyPolicy: 'src/privacypolicy/index.html'
+      }
+    }  
+  },
+  css: {
+    minify: true, // Enable CSS minification
+  },
+  plugins: [
+    createHtmlPlugin({
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true,
+        minifyJS: true,
+      },
+    }),
+  ],
+});
